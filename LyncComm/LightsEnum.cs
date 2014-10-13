2d8a4f6f05a50dbd790db47db330b52cd1024294
CCrawford <chris@ccrawford.org>
2014-10-13 15:19:25 -0500

    New files

---
+++ LyncComm/LightsEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyncLights
+{
+    public enum LIGHTS
+    {
+        RED = 6,
+        YELLOW = 5,
+        GREEN = 4,
+        OFF = 7,
+        STATUS = 8
+    }
+}
